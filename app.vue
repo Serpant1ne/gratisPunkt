c06683ea19e1cccc492afc653bb6f0d9cc7136be
Serpant1ne <gartsmankonstantin@gmail.com>
2025-05-25 19:23:07 +0200

    First commit + dependencies

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    hello GratisPunkt
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/image',
+    'shadcn-nuxt',
+    '@nuxtjs/leaflet'
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: 'Scn',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
